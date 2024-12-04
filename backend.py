88b09a869cfc65a8334f792e60cd821d119e1c83
Vedarth_Khandve <103764711+Vedarth007@users.noreply.github.com>
2024-12-04 22:10:34 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,14 +5,7 @@ import sqlite3
 from sentence_transformers import SentenceTransformer
 import logging
 
-import os
-import subprocess
 
-try:
-    import faiss
-except ImportError:
-    subprocess.run(["pip", "install", "faiss-cpu"], check=True)
-    import faiss
 
 class Backend:
     def __init__(self, db_path='database.db'):
